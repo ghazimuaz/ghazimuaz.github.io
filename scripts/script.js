6b23619c4a8916ea3e3dedf602ea3f9a637d13d4
zedandwhite <164160591+zedandwhite@users.noreply.github.com>
2025-02-13 20:56:15 +0500

    initial Push

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const cursor = new MouseFollower();
+const profile = document.getElementById('title');
+const downloadBtn = document.getElementById('download-btn');
+const HireBtn = document.getElementById('hire-btn');
+
+profile.addEventListener('mouseenter', () => {
+    cursor.setImg('./assets/img/profile.png')
+});
+
+profile.addEventListener('mouseleave', () => {
+    cursor.removeImg()
+});
+
+downloadBtn.addEventListener('mouseenter', () => {
+    cursor.setState('.-lg')
+});
